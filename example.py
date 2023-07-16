335f80b63dd73730238e1b74f5de961efa8d07e4
roman <skimprem@gmail.com>
2023-07-17 00:38:06 +0600

    Add cli interface

--- example.py
+++ example.py
@@ -7,10 +7,32 @@ if __name__ == "__main__":
   More complex analysis requires pygtide / ocl parameters
   """
 
-  data = DataLoader.load("CG5", "example/CG5.dat")
-  result = data.invert(2)
-  result.plot("CG5.pdf")
+  data = DataLoader.load("CG6", "/home/roman/gitrepo/simple_grav_proc/example/vertical_gradient/CG-6_0461_VG_1253.dat")
+  data.setLocations("example/locations/stations.csv")
+  result = data.invert(degree=1, anchor="1", tide="Longman")
+  result.plot("CG-6_0461_VG_1253.pdf")
+  ##result.plotResiduals()
+  #result.differences
+  result.save("CG-6_0461_VG_1253.txt")
 
-  data = DataLoader.load("CG6", "example/CG6.dat")
-  result = data.invert(2, anchor="40")
-  result.plot("CG6.pdf")
+  #data = DataLoader.load("CG6", "example/CG-6_0461_VG_1253.dat")
+  #data.setLocations("example/locations/stations.csv")
+  #result = data.invert(degree=6, anchor="1", tide="Longman")
+  #result.plot("CG-6_0461_VG_1253.pdf")
+  ##result.plotResiduals()
+  #result.differences
+  #result.save("CG-6_0461_VG_1253.txt")
+
+  #data = DataLoader.load("CG5", "example/CG5.dat")
+  #result = data.invert(1)
+  #result.plot("CG5.pdf")
+  #result.plotResiduals()
+  #result.differences
+  #result.save("CG5.txt")
+
+  #data = DataLoader.load("CG6", "example/CG6.dat")
+  #result = data.invert(1, anchor="40")
+  #result.plot("CG6.pdf")
+  #result.plotResiduals()
+  #result.differences
+  #result.save("CG6.txt")
